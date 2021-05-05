3389978f61fed916a77a637565f952cf2427fa12
Si3ver <izhouwl@163.com>
2021-05-05 15:36:14 +0800

    feat: 从接口获取天气数据

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 + semantic-ui-react 样式库
 + dayjs 时间处理
 
+## 知识点
+
+1. 获取位置信息 [Navigator.geolocation](https://developer.mozilla.org/zh-CN/docs/Web/API/Navigator/geolocation)
+
 ## 启动应用
 
 ```sh
--- src/App.js
+++ src/App.js
@@ -4,20 +4,30 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    })
-
-    console.log('Latitude is: ', lat);
-    console.log('Longitude is: ', long);
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      })
+      if (!lat || !long) {
+        return
+      }
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result);
+        console.log(result);
+      })
+    }
+    fetchData();
   }, [lat, long]);
 
   return (
     <div className="App">
-
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 }
